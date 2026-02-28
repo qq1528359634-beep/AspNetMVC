6e2efe92e6e4fe136392e3030ab1c2f1c29f83f7
KANSEI CHIN <qq1528359634@gmail.com>
2026-02-28 22:12:10 +0900

    add a Db context class

---
+++ SelfAspNet/DB/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SelfAspNet_MVC.Models;
+
+namespace SelfAspNet_MVC.DB
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+                
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ SelfAspNet/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace SelfAspNet_MVC.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Isbn { get; set; } = string.Empty;
+        public string  Title { get; set; }= string.Empty;
+        public int price { get; set; }
+        public string Publisher { get; set; } = string.Empty;
+        public DateTime Published { get; set; }
+        public bool Sample { get; set; } 
+    }
+}
