5a9fe5b64dd8cb7f2bc4d79785d61c083524be07
KANSEI CHIN <qq1528359634@gmail.com>
2026-02-28 21:00:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AspNetMVC
+a demo of MVC 
---
+++ SelfAspNet/Models/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SelfAspNet.Models
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //右键Show add viewm
+        public IActionResult Show()
+        {
+            ViewData["Message"] = "Hello World";
+            //razorテンプレートを呼び出す
+            return View();
+        }
+    }
+}
